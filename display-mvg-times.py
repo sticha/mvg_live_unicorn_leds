e3230c5e5d8647e2b942c3b3a51e7ca7f97bbc05
Jonas Sticha <jonas.sticha@tum.de>
2017-11-12 13:50:55 +0000

    Update to Tram at Ackermannstraße

--- display-mvg-times.py
+++ display-mvg-times.py
@@ -11,39 +11,51 @@ unicorn.brightness(0.6)
 width,height=unicorn.get_shape()
 
 
-def getCurrentTimes():
+def getCurrentTransports():
     foo = MVGLive.MVGLive()
-    liveData = foo.getlivedata("Fröttmaning")
+    liveData = foo.getlivedata("Ackermannstraße")
     times = []
-    for subway in liveData:
-        if subway['direction'] == "1":
-            times.append(subway['time'])
+    for transport in liveData:
+        if transport['product'] == "Tram":
+            goesToPetuelring = False
+            if transport['destination'] == "Petuelring":
+                goesToPetuelring = True
+            times.append((transport['time'], goesToPetuelring))
     return times
+
+departingColour = (255, 0, 0) # red
+evenTimeColour = (0, 255, 255) # light blue
+goingNorthColour = (0, 0, 255) # blue
+goingSouthColour = (0, 255, 0) # green
+defaultColour = (255, 102, 0) # orange
 
 def visualizeTimes(width, height):
     unicorn.clear()
     i = 0
-    for time in getCurrentTimes():
+    for transport in getCurrentTransports():
         if i >= height:
             break
+        time = transport[0]
+        goingToPetuelring = transport[1]
         for t in range((time/2) + 1):
-            bb = 255
             if t >= width:
                 break
-            rr = 0
-            if t <= 2:
-                rr = 255
-            gg = 0
-            if t == ((time/2)):
-                if time%2 == 0:
-                    gg = 255
+            colourToSet = defaultColour
             if time == 0:
-                rr = 255
-                gg = 69
-                bb = 0
-            unicorn.set_pixel(t, i, rr, gg, bb)
+                colourToSet = departingColour
+            elif t == ((time/2)) and time%2 == 0:
+                colourToSet = evenTimeColour
+            elif t <= 2:
+                if goingToPetuelring:
+                    colourToSet = goingNorthColour
+                else:
+                    colourToSet = goingSouthColour
+            setColourAt(t, i, colourToSet)
         i = i+1
     unicorn.show()
+
+def setColourAt(t, i, colour):
+    unicorn.set_pixel(t, i, colour[0], colour[1], colour[2])
 
 while 1==1:
     visualizeTimes(width, height)
